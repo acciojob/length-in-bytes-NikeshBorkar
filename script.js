32888b961829692cbec63fc239ca08387773ee88
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-30 22:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+
+  // Return the size of the Blob object in bytes
+  return blob.size;
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
